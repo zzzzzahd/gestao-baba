c65f6128d205959ea6d4c99019114c8a943c481a
zzzzzahd <zharickdiias@gmail.com>
2026-02-03 16:41:18 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App'; // Importação padrão (Vite resolve .jsx)
-import './styles/global.css'; 
+import App from './App';
+import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
